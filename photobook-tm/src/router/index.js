bc131aba27fc3fe867598ec5bf50befed946e5dc
Erik Martinez <erik.martinez@paradyme.us>
2020-10-31 13:25:02 -0400

    save

---
+++ photobook-tm/src/router/index.js
@@ -0,0 +1,61 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import SignUp from "../views/SignUp.vue";
+import AlbumsDetailPage from "../views/AlbumsDetailPage.vue";
+import AlbumsPage from "../views/AlbumsPage.vue";
+import { Auth } from "aws-amplify";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp
+  },
+  {
+    path: "/album/:id",
+    name: "AlbumsDetailPage",
+    component: AlbumsDetailPage,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: "/albums",
+    name: "AlbumsPage",
+    component: AlbumsPage,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const isAuthenticated = await Auth.currentUserInfo();
+
+  if (requiresAuth && !isAuthenticated) {
+    next("/");
+  } else {
+    next();
+  }
+});
+
+export default router;
